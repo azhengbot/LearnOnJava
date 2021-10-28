d037b6f6040be60ab67d01c19a06c33aeb1ceb3f
xuhaoqing <xuhaoqing@lattebank.com>
2021-10-28 15:49:36 +0800

    add: init mvn

--- collections/src/main/java/collections/FinalFields.java
+++ collections/src/main/java/collections/FinalFields.java
@@ -1,3 +1,4 @@
+
 // collections/FinalFields.java
 // (c)2021 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
@@ -5,8 +6,10 @@
 // {NewFeature} Since JDK 16
 import java.util.*;
 
-//record FinalFields(int i) {
-//  int timesTen() { return i * 10; }
-//  // void tryToChange() { i++; } // Error:
-//  // cannot assign a value to final variable i
-//}
+record FinalFields(int i) {
+    int timesTen() {
+        return i * 10;
+    }
+    // void tryToChange() { i++; } // Error:
+    // cannot assign a value to final variable i
+}
