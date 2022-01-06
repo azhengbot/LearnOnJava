a9502e6dc2399941103cec3aa3f8482d02401843
xuhaoqing <xuhaoqing@lattebank.com>
2022-01-06 14:01:23 +0800

    validating

--- validating/src/main/java/validating/CircularQueueException.java
+++ validating/src/main/java/validating/CircularQueueException.java
@@ -1,10 +1,10 @@
-package validating;// validating/CircularQueueException.java
+// validating/CircularQueueException.java
 // (c)2021 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
+package validating;
 
-public class
-CircularQueueException extends RuntimeException {
+public class CircularQueueException extends RuntimeException {
   public CircularQueueException(String why) {
     super(why);
   }
